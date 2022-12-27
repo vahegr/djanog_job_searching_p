bfb40ef957d8b1fc1d8e5a994ed3766d448f36c8
Vahe Grigorian <vahegrigorian447@gmail.com>
2022-12-27 21:25:14 +0330

    authentication views craeted

--- account/views.py
+++ account/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LogInForm, UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from .models import User
 
-# Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'account/signUp.html', {'form': form})
+
+
+def log_in_acc(request):
+    if request.user.is_authenticated is True:
+        return redirect('home:home')
+
+    if request.method == "POST":
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            user = User.objects.get(username=form.cleaned_data.get('username'))
+            login(request, user)
+            return redirect('home:home')
+    else:
+        form = LogInForm()
+    return render(request, "account/login.html", context={'form': form})
+
+
+def log_out_acc(request):
+    logout(request)
+    return redirect('home:home')
